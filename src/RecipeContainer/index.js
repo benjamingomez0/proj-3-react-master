37333835f52da2995eac00654efaeef822668a24
benjamingomez0 <benjamingomez0@gmail.com>
2019-11-21 13:02:25 -0800

    created index component

---
+++ src/RecipeContainer/index.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import RecipeList from '../RecipeList'
+
+class RecipeContainer extends Component{
+    state={
+        recipes:[]
+    }
+    componentDidMount(){
+        this.getRecipes()
+    }
+    getRecipes= async () =>{
+        try{
+        const recipes = await fetch(`${process.env.REACT_APP_API_URL}/recipes/`);
+        const parsedRecipes = await recipes.json()
+        this.setState({
+            recipes:
+            parsedRecipes.data
+        })
+        }
+        catch(err)
+        {
+            console.log(err)
+        }
+    }
+    render(){
+    return(
+        <RecipeList recipes = {this.state.recipes}/>
+    )
+    }
+}
+
+export default RecipeContainer
