aef8fdec2ee9e9564774b738f4b07278ce440dc9
benjamingomez0 <benjamingomez0@gmail.com>
2019-11-22 09:30:31 -0800

    clean comments out from app.js

--- src/App.js
+++ src/App.js
@@ -1,55 +1,14 @@
 import React, { Component } from 'react'
-// import NewRecipe from "./NewRecipe"
 import './App.css';
-// import Register from './Register'
-// import UserShow from "./ShowUser"
-// import NavBar from "./Nav"
-// import Login from "./Login"
-
 import RecipeContainer from './RecipeContainer'
 
-class App extends Component {
-  state = {
-    // currentUser:{},
-    // loginModal:false
-  }
-
-  // doUpdateCurrentUser=user=>{
-  //   this.setState({
-  //     currentUser : user.data,
-  //     loginModal:false
-  //   })
-  // }
-  // showLoginModal=()=>{
-  //   this.setState({
-  //     loginModal:true
-  //   })
-  // }
-  // hideLoginModal=()=>{
-  //   this.setState({
-  //     loginModal:false
-  //   })
-  // }
-  
-  render(){
+const App =()=>{
     return (
       <div className="App">
-
-        {/* <NavBar showLoginModal={this.showLoginModal} currentUser={this.state.currentUser}/>
-        {
-          this.state.loginModal?
-        <Login  doUpdateCurrentUser={this.doUpdateCurrentUser}/> :null
-        }
-
-         <NewRecipe UserId={this.state.currentUser.id}/>   
-        <UserShow user={this.state.currentUser}/>
-        
-        <Register doUpdateCurrentUser = {this.doUpdateCurrentUser}/>     */}
-        
         <RecipeContainer />
       </div>
     )
-  }
+  
 }
 
 export default App;
